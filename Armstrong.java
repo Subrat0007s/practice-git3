adc3711e5ae6b6242ea2a9bd19652419fbc4ff6c
Subrat0007s <133476978+Subrat0007s@users.noreply.github.com>
2024-05-20 15:39:09 +0530

    Update Armstrong.java

--- Armstrong.java
+++ Armstrong.java
@@ -8,25 +8,25 @@ import java.util.Scanner;
 }
 class ArmstrongImpln implements Armstrong{
     public String isArmstrong(int a){
-		int t=a,len=0;
-		while (t!=0){
-			t=t/10;
-			len++;
+	int t=a,len=0;
+	while (t!=0){
+		t=t/10;
+		len++;
+	}
+	int arm=0, t1=a,rem=0;
+	while(t1!=0){
+		rem=t1%10;
+		int mul=1;
+		for(int i=0;i<len;i++){
+			mul=mul*rem;
 		}
-		int arm=0, t1=a,rem=0;
-		while(t1!=0){
-			rem=t1%10;
-			int mul=1;
-			for(int i=0;i<len;i++){
-				mul=mul*rem;
-			}
-			arm=arm+mul;
-			t1=t1/10;
-		}
-		if(a==arm){
-			return "Armstrong number";
-		}
-		return "Not an Armstrong number";
+		arm=arm+mul;
+		t1=t1/10;
+	}
+	if(a==arm){
+		return "Armstrong number";
+	}
+	return "Not an Armstrong number";
     }
 }
 
